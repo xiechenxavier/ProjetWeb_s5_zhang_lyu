7d7144762da66e3ebdbf789b252b2cf14b7058dd
xiechen zhang <xiechen.zhang4@etu.univ-lorraine.fr>
2019-10-01 16:48:56 +0200

    create project

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="./CSS/style.css" />
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div class="menu">
+            <ul class="navbar">
+                <li>Accueil</li>
+                <li>Manoir des noir</li>
+                <li>Eglise</li>
+                <li>Chateau de Lachaise</li>
+                <li>Château d'idogne</li>
+                <li>Domaine de la Quérye</li>
+                <li>Centre National du Costume de Scène</li>
+            </ul>
+        </div>
+        <section>
+            <div>
+
+            </div>
+        </section>
+    </body>
+</html>
