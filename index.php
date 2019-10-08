f7ba5f6bc560f6045ff0141ea319cd9f56cbc8c1
xiechen zhang <xiechen.zhang4@etu.univ-lorraine.fr>
2019-10-08 14:38:34 +0200

    create LieuParLieu.php and update index

--- index.php
+++ index.php
@@ -14,14 +14,16 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
+        <div class="bandeau">
+			 <div class="petitPanier"><table>Billets en vente exclusivement sur les lieux du festival: Monétay, Monteignet, Veauce  du 2 au 6 août dès 11h00 et le 6 août à Moulins de 19h00 à 20h00.
+									Attention! à Moulins le début du spectacle à 20h00. </table></div><!-- class="petitPanier"-->							
+										<h1>
+				Théâtres de Bourbon&#8239;: les spectacles
+			</h1>
         <div class="menu">
             <ul class="navbar">
-                <li>Accueil</li>
-                <li>Manoir des noir</li>
-                <li>Eglise</li>
-                <li>Chateau de Lachaise</li>
-                <li>Château d'idogne</li>
-                <li>Domaine de la Quérye</li>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="./LieuParLieu.php">Lieu par Lieu</a></li>
                 <li>Centre National du Costume de Scène</li>
             </ul>
         </div>
