3cac1967bb7200b467394ef2a9a0a73954e51996
viviox <45592671+viviox@users.noreply.github.com>
2019-10-08 14:24:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ and open the template in the editor.
                 <li>Domaine de la Quérye</li>
                 <li>Centre National du Costume de Scène</li>
             </ul>
-        </div>
+        </div>1
         <section>
             <div>
 
