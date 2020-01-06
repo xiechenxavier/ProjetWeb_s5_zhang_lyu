c6c9957a2a4c114fa02718662af0b3a38dadb1b9
xiechen zhang <xiechen.zhang4@etu.univ-lorraine.fr>
2020-01-06 15:11:00 +0100

    Accueil

--- Accueil.php
+++ Accueil.php
@@ -6,80 +6,33 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <title>TODO supply a title</title>
         <meta charset="UTF-8">
-        <title>Accueil</title>
-        <link rel="stylesheet" type="text/css" href="./CSS/style.css" />
-        <link rel="stylesheet" type="text/css" href="./CSS/style2.css" />
-        <script src="//apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="//apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="./JS/Front.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-    <body>
-        <div class="bandeau">
-            <div class="petitPanier"><table>Billets en vente exclusivement sur les lieux du festival: Monétay, Monteignet, Veauce  du 2 au 6 août dès 11h00 et le 6 août à Moulins de 19h00 à 20h00.
-                    Attention! à Moulins le début du spectacle à 20h00. </table></div><!-- class="petitPanier"-->							
-            <h1>
-                Théâtres de Bourbon&#8239;: les spectacles
-            </h1>
-            <div class="menu">
-                <ul class="navbar">
-                    <li><a href="./Accueil.php">Accueil</a></li>
-                    <li><a href="./FirstPart/LieuParLieu.php">Lieu par Lieu</a></li>
-                    <li><a href="./FirstPart/JourParJour.php">Jour par Jour</a></li>
-                    <li><a href="./FirstPart/TroupeParTroupe.php">Jour par Jour</a></li>
-                    <li><a href="./Reservation.php">Reserver les billes</li>
-                    <li><a href="./Canvas.php">Finances du festival</li>
-                </ul>
-            </div>
-            <div class="btn_hide">
-                <img  src="./images/flaish2.png">
-            </div>
+    <link rel="stylesheet" type="text/css" href="./CSS/demo.css" />
 
-            <div class="btn_show">
-                <img  src="./images/flaish_r2_c2.png">
-            </div>
-        </div>
-        <<main>
+    <!--必要样式-->
+    <link rel="stylesheet" type="text/css" href="./CSS/style2_1.css" />
+</head>
+<body>
 
-            <div class="decalage">
-                <h1><a href="https://www.theatresdebourbon.com/Festival2018ProgrammationVueGlobale.php">Du 2 au 6 août 2019</a>
-                </h1>
-                <h2><a href="https://www.theatresdebourbon.com/Festival2018ProgrammationParSpectacle.php">Une quinzaine de spectacles</a>, 
-                    <a href="https://www.theatresdebourbon.com/Festival2018ProgrammationParJour.php">une quarantaine de représentations</a>
-                </h2>
-                <h2>Des plus grands auteurs 
-                    (<!--<a href="Festival2018ProgrammationParSpectacle.php#labelSpectacle_10371              z">-->
-                    Molière<!--</a>-->,					
-                    <!-- <a href="Festival2018ProgrammationParSpectacle.php#labelSpectacle_10372              z">-->Shakespeare
-                    <!--</a>-->...) 
-                    aux créations <a href="https://www.theatresdebourbon.com/Festival2018ProgrammationParSpectacle.php#labelSpectacle_10402%20%20%20%20%20%20%20%20%20%20%20%20%20%20z">les plus originales</a>,
-                </h2>
-                <h2> En plein air, les pieds dans l'herbe mais dans 
-                    <a href="https://www.theatresdebourbon.com/Festival2018ProgrammationParLieu.php">des cadres exceptionnels</a>, 
-                </h2>
-                <h2> <a href="https://www.theatresdebourbon.com/Festival2019Tarifs.php">en toute simplicité</a>, chaleur et convivialité, 
-                    <a href="https://www.theatresdebourbon.com/Festival2018ProgrammationParLieu.php">au cœur de la France</a>.
-                </h2>
+    <ul class="cb-slideshow">
+        <li><span>Image 01</span><div><h3>Festival de théatres Bourbon</h3></div></li>
+        <li><span>Image 02</span><div><h3>Des théatres</h3></div></li>
+        <li><span>Image 03</span><div><h3>lieus·Villes</h3></div></li>
+        <li><span>Image 04</span><div><h3>lieus·Villes</h3></div></li>
+        <li><span>Image 05</span><div><h3>lieus·Villes</h3></div></li>
+        <li><span>Image 06</span><div><h3>lieus·Villes</h3></div></li>
+    </ul>
 
-                <h1><a href="https://www.theatresdebourbon.com/Festival2018ProgrammationVueGlobale.php">Changeons quelque chose à notre vie !</a></h1>
-                <h2 class="adroite"><a href="https://www.theatresdebourbon.com/presentation.php">Théâtres de bourbon</a></h2>
-                <div class="adroite"><a href="https://www.theatresdebourbon.com/index.php"><img class="petiteVignette" src="./Theatres de Bourbon ; Accueil_files/logo.jpg" alt="[logo de l&#39;association vers l&#39;accueil du site]" width="100%" height="100%" decoding="low">
-                    </a>
+    <p class="codrops-demos">
+        <a href="#">Accueil</a>
+        <a href="./FirstPart/LieuParLieu.php">Lieu par Lieu</a>
+        <a href="./FirstPart/JourParJour.php">Jour par Jour</a>
+        <a href="./FirstPart/TroupeParTroupe.php">Troupe par Troupe</a>
+        <a href="./Reservation.php">Reserver les billes</a>
+    </p>
 
-                </div><!--  adroite -->	
-
-                information de derniere minute: à cause du spectacle de son et lumiere de Moulins, la représentation de Tartuffe ou l'imposteur au CNCS de Moulins le 6 août 2019 initialement prévu à 20h30 est avancé à 20h00. 
-
-                <iframe src="./Theatres de Bourbon ; Accueil_files/PdfFlyerA4RectoVerso.html" width="640" height="480"></iframe>
-            </div><!--  decalage -->	
-
-
-        </main> 
-        <footer>
-
-            index
-            <!-- Signer et dater la page, c'est une question de politesse! -->
-            <address>Page conçue par Sylvie Delaët, choix éditoriaux Pierre Deusy et Jules Reigneaud,  vendredi 20 décembre 2018</address>
-        </footer>
-    </body>
+</body>
 </html>
